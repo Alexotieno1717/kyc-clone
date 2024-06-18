da77fcc04cc624783bfd289666ba85ddc014738d
Alexotieno1717 <alexotieno900@gmail.com>
2024-06-18 16:02:46 +0300

    [feat]: dropdonw Ui for Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from "next/link";
 
 
@@ -11,9 +11,11 @@ const Links = [
 
 const Header = () => {
 
+    const [toggle, setToggle]  = useState(false);
+
     return (
         <>
-            <nav className="bg-[#206A3F] sticky top-0 z-50">
+            <nav className="bg-[#206A3F] sticky top-0 z-50 mb-3">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-16 items-center justify-between">
                         <div className="flex items-center">
@@ -51,36 +53,44 @@ const Header = () => {
                         </div>
                         <div className="-mr-2 flex md:hidden">
 
-                            <button type="button"
-                                    className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                    aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button"
+                                    onClick={() => setToggle(!toggle)}
+                                    className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 transition duration-700 ease-in-out"
+                                    aria-controls="mobile-menu" aria-expanded={toggle}>
                                 <span className="absolute -inset-0.5"></span>
                                 <span className="sr-only">Open main menu</span>
 
-                                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                                </svg>
-
-                                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
+                                {!toggle ? (
+                                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                         stroke="currentColor" aria-hidden="true">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                                    </svg>
+                                ) : (
+                                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                         stroke="currentColor" aria-hidden="true">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                                    </svg>
+                                )}
                             </button>
+
                         </div>
                     </div>
                 </div>
 
 
-                <div className="md:hidden" id="mobile-menu">
-                    <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-
-                        <a href="#"
-                           className="bg-[#206A3F]/70 shadow-lg text-white block rounded-md px-3 py-2 text-base font-medium"
-                           aria-current="page">Home</a>
+                {toggle && (
+                    <div className="md:hidden" id="mobile-menu">
+                        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                            {Links.map((link, index) => (
+                                <a key={index}
+                                   href={link.url}
+                                   className="bg-[#206A3F]/70 shadow-lg text-white block rounded-md px-3 py-2 text-base font-medium"
+                                   aria-current="page">{link.name}</a>
+                            ))}
+                        </div>
                     </div>
-                </div>
+                )}
             </nav>
 
         </>
